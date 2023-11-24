a6768b6af28240b55094094bac2301004db11713
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-24 12:54:26 +0200

    Debugging dll issue

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -2,6 +2,6 @@
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
-locate b '\libdb2.so'
+find / -name "libdb2.so"
 
 export LD_LIBRARY_PATH="./Frends.IBMDB2.ExecuteQuery/clidriver/lib"
