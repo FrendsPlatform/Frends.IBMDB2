3aa026561268b07cd36cb5b07439ed57aac568c4
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-24 13:49:32 +0200

    Debugging dll issue

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
+# docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
 sudo find / -type f -name "libdb2.so"
 
 # Use find to locate the file and store the result in an array
-mapfile -t myvariable < <(sudo find / -type f -name "libdb2.so")
+mapfile -t myvariable=$(sudo find / -type f -name "libdb2.so")
 
 # Check if the array is not empty
 if [ "${#myvariable[@]}" -gt 0 ]; then
