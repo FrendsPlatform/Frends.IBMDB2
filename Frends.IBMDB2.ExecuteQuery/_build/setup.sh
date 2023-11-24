c1c7ad26221ab5f918cf1966a3b0ac6584eed7b9
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-24 15:25:03 +0200

    Debugging dll issue

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -2,22 +2,27 @@
 
 # docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
-sudo find / -type f -name "libdb2.so"
+#sudo find / -type f -name "libdb2.so"
 
 # Use find to locate the file and store the result in an array
-myvariable=$(sudo find / -type f -name "libdb2.so")
+#myvariable=$(sudo find / -type f -name "libdb2.so")
 
 # Check if the array is not empty
-if [ "${#myvariable[@]}" -gt 0 ]; then
+#if [ "${#myvariable[@]}" -gt 0 ]; then
     # Print all paths
-    echo "Paths found:"
-    for path in "${myvariable[@]}"; do
-        echo "$path"
-    done
+    #echo "Paths found:"
+    #for path in "${myvariable[@]}";
+        #{ echo "$path" }
+    #done
 
     # Set LD_LIBRARY_PATH to the first path
-    export LD_LIBRARY_PATH="${myvariable[0]}"
-    echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
-else
-    echo "File not found"
-fi
+    #export LD_LIBRARY_PATH=${myvariable[0]}
+    #echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
+#else
+    #echo "File not found"
+#fi
+
+sudo find / -name "libdb2.so" -print0 | while read -d $'\0' file
+do
+	echo "$file"
+done
