f3b868eb39dede3d39c5985d5280283f91739746
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-24 13:27:32 +0200

    Debugging dll issue

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -2,7 +2,7 @@
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
-myvariable = $(sudo find / -type f -name "libdb2.so")
+myvariable=$(sudo find / -type f -name "libdb2.so")
 
 echo "$myvariable"
 
