3b92851b9243d116f9af2f9273130a916d0f003c
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-24 15:49:37 +0200

    Debugging dll issue

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
+#docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
 #sudo find / -type f -name "libdb2.so"
+
+echo "Start file find"
 
 # Use find to locate the file and store the result in an array
 filePaths=$(sudo find / -type f -name "libdb2.so")
 
-echo ${filePaths[0]}
+echo "${filePaths[0]}"
 
 # Check if the array is not empty
 if [ "${#filePaths[@]}" -gt 0 ]; then
     # Print all paths
     echo "Paths found:"
-    for path in "${filePaths[@]}";
-        { echo "$path" }
+    for path in "${filePaths[@]}"; do
+        echo "$path"
     done
 
     # Set LD_LIBRARY_PATH to the first path
