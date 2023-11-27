0a6aa7b0f086fede3bc03db1323a2554d7c5886d
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-27 09:50:04 +0200

    Modified the setup script

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -2,23 +2,9 @@
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
-# Use find to locate the file and store the result in an array
-
-basePath=$(sudo find $HOME -name "net.ibm.data.db2-lnx")
-
-echo "Base path: $basePath"
-
-clidriver=$(sudo find $basePath -name "clidriver")
-
-echo "Clidriver: $clidriver"
-
-lib=$(sudo find $clidriver -name "lib")
-
-echo "Lib: $lib"
-
 # Set LD_LIBRARY_PATH to the first path
 
-export LD_LIBRARY_PATH="$lib"
+export LD_LIBRARY_PATH="./_build/lib"
 
 echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
 
