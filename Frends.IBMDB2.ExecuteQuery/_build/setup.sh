d20e1cbaae2399244836086318df9ec89c074566
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-27 10:28:28 +0200

    Debugging sh script

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -2,11 +2,23 @@
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
+# Use find to locate the file and store the result in an array
+
+nuget=$(sudo find / -name ".nuget")
+
+echo "Nuget: $nuget"
+
+basePath=$(sudo find $HOME -name "net.ibm.data.db2-lnx")
+
+clidriver=$(sudo find ${basePath[0]} -name "clidriver")
+
+lib=$(sudo find ${clidriver[0]} -name "lib")
+
+echo $lib
+
 # Set LD_LIBRARY_PATH to the first path
 
-export LD_LIBRARY_PATH="$PWD/_build/net.ibm.data.db2-lnx/7.0.0.200/buildTransitive/clidriver/lib"
+export LD_LIBRARY_PATH="${lib[0]}"
 
 echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
-
-ls $LD_LIBRARY_PATH
 
