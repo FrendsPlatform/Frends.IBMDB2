63a07ba4d5198134af903756d50f7dc47741cf01
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-27 09:34:41 +0200

    Modified the setup script

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
-#docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
-
-#sudo find / -type f -name "libdb2.so"
-
-echo "Start file find"
+docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ../Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
 # Use find to locate the file and store the result in an array
-filePaths=$(sudo find / -type f -name "libdb2.so")
 
-echo "Files: "
-echo "${filePaths[0]}"
+basePath=$(sudo find $HOME -name "net.ibm.data.db2-lnx")
 
-echo "Files count"
-echo "${filePaths[@]}"
+clidriver=$(sudo find ${basePath[0]} -name "clidriver")
 
-# Check if the array is not empty
-if [ ${#filePaths[@]} -gt 0 ]; then
-    # Print all paths
-    echo "Paths found:"
-    for path in $filePaths; do
-        echo $path
-    done
+lib=$(sudo find ${clidriver[0]} -name "lib")
 
-    # Set LD_LIBRARY_PATH to the first path
-    export LD_LIBRARY_PATH=${filePaths[0]}
-    echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
-else
-    echo "File not found"
-fi
+echo $lib
+
+# Set LD_LIBRARY_PATH to the first path
+
+export LD_LIBRARY_PATH="${lib[0]}"
+
+echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
+
