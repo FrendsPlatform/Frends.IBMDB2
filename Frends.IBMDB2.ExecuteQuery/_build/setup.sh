e53b03c1c1555842a1e9bf79615d5facdefb795c
Riku Virtanen <virtanen.riku91@gmail.com>
2023-11-27 09:38:52 +0200

    Modified the setup script

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ../Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
+docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
 # Use find to locate the file and store the result in an array
 
