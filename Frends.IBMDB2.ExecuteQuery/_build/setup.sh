71d93d8d02827ed6422b9c014c4bdb16c67cc314
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-06-12 12:22:29 +0200

    sudo

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get install -y libxml2-dev
+sudo apt-get update
+sudo apt-get install -y libxml2-dev
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
