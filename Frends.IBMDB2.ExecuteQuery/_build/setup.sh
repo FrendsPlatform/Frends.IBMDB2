81008de39cfff7bd3ee3a8a90e9c42074a6296fc
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-06-13 13:20:30 +0200

    debug ls ld path

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -7,18 +7,20 @@ dotnet build
 
 # Use find to locate the file and store the result in an array
 
-basePath=$(sudo find / -name "net.ibm.data.db2-lnx")
+basePath=$(find / -name "net.ibm.data.db2-lnx")
 echo "Base path: $basePath"
 
-clidriver=$(sudo find / -name "clidriver")
+clidriver=$(find / -name "clidriver")
 echo "Clidriver: $clidriver"
 
-lib=$(sudo find $clidriver -name "lib")
+lib=$(find $clidriver -name "lib")
 echo "Lib: $lib"
 
 # Set LD_LIBRARY_PATH to the first path
 export LD_LIBRARY_PATH="$lib"
 echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
+echo "List of files in LD_LIBRARY_PATH"
+ls $LD_LIBRARY_PATH
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
