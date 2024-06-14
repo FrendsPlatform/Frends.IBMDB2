dc927b310a55f33fe80a4e1fc83151b38809e108
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-06-14 11:32:32 +0200

    small fix to run tests

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -18,7 +18,4 @@
 echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
 echo "DAY set to: $DAY_OF_WEEK"
 
-
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
-
-# dotnet test --collect:"XPlat Code Coverage"
