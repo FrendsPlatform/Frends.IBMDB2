02d4b8cb314bdabf4943d1a973be3f39305132f9
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-06-14 10:37:24 +0200

    fix env

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -15,7 +15,7 @@
 
 # # Set LD_LIBRARY_PATH to the first path
 # export LD_LIBRARY_PATH="$lib"
-# echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
+echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
 
 docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 
