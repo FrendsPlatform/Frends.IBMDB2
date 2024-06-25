db3d39c4f02b61522ac702de0382a9f7cb5e102f
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-06-25 15:12:16 +0200

    try without sudo

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -15,7 +15,7 @@
 
 # # Set LD_LIBRARY_PATH to the first path
 # export LD_LIBRARY_PATH="$lib"
-echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
-echo "DAYOFWEEK set to: $DAY_OF_WEEK"
+# echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
+# echo "DAYOFWEEK set to: $DAY_OF_WEEK"
 
-docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
+# docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
