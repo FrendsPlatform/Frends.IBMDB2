cbb9ab4eeedb54f664880f736820920733756f21
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-06-26 08:07:42 +0200

    remove privileged

--- Frends.IBMDB2.ExecuteQuery/_build/setup.sh
+++ Frends.IBMDB2.ExecuteQuery/_build/setup.sh
@@ -20,3 +20,4 @@
 
 # docker run -h db2server --name db2server --restart=always --detach --privileged=true -p 50000:50000 --env-file ./Frends.IBMDB2.ExecuteQuery.Tests/lib/env_list.txt -v $PWD:/database icr.io/db2_community/db2
 #win test
+#remove privileged
